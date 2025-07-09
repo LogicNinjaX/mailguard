4bdf11e178952af383c6934e16d0ee3ae937e1f4
Nitish <mail.logicninjax@gmail.com>
2025-07-09 13:55:22 +0530

    add email category entity

---
+++ src/main/java/com/example/mailguard/entity/EmailCategories.java
@@ -0,0 +1,91 @@
+package com.example.mailguard.entity;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "email_categories",
+        uniqueConstraints =
+                @UniqueConstraint(
+                        name = "UK_email_categories_category_name",
+                        columnNames = {"categoryName"}
+                )
+)
+public class EmailCategories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID categoryId;
+
+    @Column(nullable = false)
+    private String categoryName;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isActive = false;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public EmailCategories() {
+    }
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(UUID categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
