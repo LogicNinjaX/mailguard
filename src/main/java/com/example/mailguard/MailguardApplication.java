cf60c7ff2ff3544fbf3f89a323e8d5e8940b5324
Nitish <mail.logicninjax@gmail.com>
2025-07-09 13:18:45 +0530

    first commit

---
+++ src/main/java/com/example/mailguard/MailguardApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailguardApplication.class, args);
+	}
+
+}
