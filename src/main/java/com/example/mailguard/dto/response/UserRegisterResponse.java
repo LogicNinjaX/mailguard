8147ff750a51746eaa5548c3d6265937691b5ca6
Nitish <mail.logicninjax@gmail.com>
2025-07-10 14:32:53 +0530

    add api request/response dto

---
+++ src/main/java/com/example/mailguard/dto/response/UserRegisterResponse.java
@@ -0,0 +1,67 @@
+package com.example.mailguard.dto.response;
+
+import com.example.mailguard.enums.UserRole;
+
+import java.time.LocalDateTime;
+
+public class UserRegisterResponse {
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private UserRole userRole;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+}
