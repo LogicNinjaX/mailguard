b50600cad52f4596389c79fa55ec81465f69c5a1
Nitish <mail.logicninjax@gmail.com>
2025-07-11 19:41:00 +0530

    add service methods

--- src/main/java/com/example/mailguard/service/UserProfileService.java
+++ src/main/java/com/example/mailguard/service/UserProfileService.java
@@ -1,10 +1,22 @@
 package com.example.mailguard.service;
 
 import com.example.mailguard.dto.request.UserRegisterRequest;
+import com.example.mailguard.dto.request.UserUpdateRequest;
+import com.example.mailguard.dto.response.UserDetailsResponse;
 import com.example.mailguard.dto.response.UserRegisterResponse;
+import com.example.mailguard.dto.response.UserUpdateResponse;
 import com.example.mailguard.enums.UserRole;
+import com.example.mailguard.exception.UserNotFoundException;
+
+import java.util.UUID;
 
 public interface UserProfileService {
 
     UserRegisterResponse createUser(UserRegisterRequest request, UserRole userRole);
+
+    UserDetailsResponse getUser(UUID userId) throws UserNotFoundException;
+
+    UserUpdateResponse updateUser(UUID userId, UserUpdateRequest request) throws UserNotFoundException;
+
+    void deleteUser(UUID userId);
 }
