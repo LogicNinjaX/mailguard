ef725b41e702592883fb135a329a0169d971b2c3
Nitish <mail.logicninjax@gmail.com>
2025-07-11 19:40:29 +0530

    add custom query method

--- src/main/java/com/example/mailguard/repository/UserProfileRepository.java
+++ src/main/java/com/example/mailguard/repository/UserProfileRepository.java
@@ -1,10 +1,17 @@
 package com.example.mailguard.repository;
 
 import com.example.mailguard.entity.UserProfile;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
 
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UserProfile u WHERE u.userId = :userId")
+    int deleteUser(UUID userId);
 }
