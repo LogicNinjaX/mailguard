5d62d196c366cb7c93b64c45ed3e379d8a47bb3b
Nitish <mail.logicninjax@gmail.com>
2025-07-16 10:56:39 +0530

    add user preference service interface

---
+++ src/main/java/com/example/mailguard/service/UserPreferenceService.java
@@ -0,0 +1,21 @@
+package com.example.mailguard.service;
+
+import com.example.mailguard.dto.request.UserPreferenceSaveRequest;
+import com.example.mailguard.dto.request.UserPreferenceUpdateRequest;
+import com.example.mailguard.dto.response.UserPreferenceResponse;
+import com.example.mailguard.exception.CategoryNotFoundException;
+import com.example.mailguard.exception.UserNotFoundException;
+import com.example.mailguard.exception.UserPreferenceExistException;
+import com.example.mailguard.exception.UserPreferenceNotFoundException;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserPreferenceService {
+
+    UserPreferenceResponse savePreference(UserPreferenceSaveRequest request) throws UserPreferenceExistException, UserNotFoundException, CategoryNotFoundException;
+
+    UserPreferenceResponse updateConsent(UserPreferenceUpdateRequest request) throws UserPreferenceNotFoundException;
+
+    List<UserPreferenceResponse> getUserPreferences(UUID userId, int page, int size, String sortBy, String sortDir);
+}
