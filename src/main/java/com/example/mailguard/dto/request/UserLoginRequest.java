c99bb58afbc9f60d7dd13e02dfb83d19d2247a20
Nitish <mail.logicninjax@gmail.com>
2025-07-18 21:25:16 +0530

    add login dto

---
+++ src/main/java/com/example/mailguard/dto/request/UserLoginRequest.java
@@ -0,0 +1,28 @@
+package com.example.mailguard.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserLoginRequest {
+
+    @NotBlank(message = "user.username.not-blank")
+    private String username;
+
+    @NotBlank(message = "user.password.not-blank")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
