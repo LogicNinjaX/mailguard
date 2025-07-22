8e312147da9d82e74cd874a08c03657f8f79ac50
Nitish <mail.logicninjax@gmail.com>
2025-07-22 20:01:15 +0530

    add mapper method

--- src/main/java/com/example/mailguard/mapper/UserPreferenceMapper.java
+++ src/main/java/com/example/mailguard/mapper/UserPreferenceMapper.java
@@ -1,6 +1,7 @@
 package com.example.mailguard.mapper;
 
 import com.example.mailguard.dto.response.UserPreferenceResponse;
+import com.example.mailguard.dto.response.UserPreferenceWithUser;
 import com.example.mailguard.entity.UserPreferences;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -11,4 +12,8 @@ public interface UserPreferenceMapper {
     @Mapping(source = "userPreferences.category.categoryName", target = "categoryName")
     @Mapping(source = "userPreferences.category.description", target = "categoryDescription")
     UserPreferenceResponse toUserPreferenceResponse(UserPreferences userPreferences);
+
+    @Mapping(source = "userPreferences.user.username", target = "username")
+    @Mapping(source = "userPreferences.category.categoryName", target = "categoryName")
+    UserPreferenceWithUser toUserPreferenceWithUser(UserPreferences userPreferences);
 }
