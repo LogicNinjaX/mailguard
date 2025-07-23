86b8061e9b527706d56ef13c4abe4ba3fb9f9a8b
Nitish <mail.logicninjax@gmail.com>
2025-07-23 13:20:46 +0530

    add service interface impl

--- src/main/java/com/example/mailguard/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/mailguard/service/impl/AdminServiceImpl.java
@@ -1,9 +1,30 @@
 package com.example.mailguard.service.impl;
 
+import com.example.mailguard.repository.UserPreferencesRepository;
 import com.example.mailguard.service.AdminService;
+import com.example.mailguard.service.EmailService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
 public class AdminServiceImpl implements AdminService {
 
+    private final UserPreferencesRepository userPreferencesRepository;
+    private final EmailService emailService;
 
+    public AdminServiceImpl(UserPreferencesRepository userPreferencesRepository, EmailService emailService) {
+        this.userPreferencesRepository = userPreferencesRepository;
+        this.emailService = emailService;
+    }
+
+    public void sendEmailByCategory(String categoryName){
+        List<String> userEmails = userPreferencesRepository.getAllEmails(categoryName);
+
+        if (!userEmails.isEmpty()){
+            emailService.sendBulkEmail(userEmails, "Test message", "Email sent from spring boot");
+        }
+    }
 
 }
