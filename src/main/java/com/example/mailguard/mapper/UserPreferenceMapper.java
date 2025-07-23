babc8a39264f7d1a6f3880ae880ab87bad3d6fcb
Nitish <mail.logicninjax@gmail.com>
2025-07-23 13:19:33 +0530

    add preference mapper method

--- src/main/java/com/example/mailguard/mapper/UserPreferenceMapper.java
+++ src/main/java/com/example/mailguard/mapper/UserPreferenceMapper.java
@@ -3,6 +3,7 @@ package com.example.mailguard.mapper;
 import com.example.mailguard.dto.response.UserPreferenceResponse;
 import com.example.mailguard.dto.response.UserPreferenceWithUser;
 import com.example.mailguard.entity.UserPreferences;
+import jakarta.mail.MailSessionDefinition;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,6 +12,8 @@ public interface UserPreferenceMapper {
 
     @Mapping(source = "userPreferences.category.categoryName", target = "categoryName")
     @Mapping(source = "userPreferences.category.description", target = "categoryDescription")
+    @Mapping(source = "userPreferences.createdAt", target = "createdAt")
+    @Mapping(source = "userPreferences.updatedAt", target = "updatedAt")
     UserPreferenceResponse toUserPreferenceResponse(UserPreferences userPreferences);
 
     @Mapping(source = "userPreferences.user.username", target = "username")
