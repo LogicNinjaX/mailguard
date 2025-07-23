db979dd4b7cc53f67af23c7167a8a53ace21ee7a
Nitish <mail.logicninjax@gmail.com>
2025-07-23 13:17:33 +0530

    add custom query method

--- src/main/java/com/example/mailguard/repository/UserPreferencesRepository.java
+++ src/main/java/com/example/mailguard/repository/UserPreferencesRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserPreferencesRepository extends JpaRepository<UserPreferences, UUID> {
@@ -15,4 +16,7 @@ public interface UserPreferencesRepository extends JpaRepository<UserPreferences
 
     @Query("SELECT up FROM UserPreferences up WHERE up.user.userId = :userId")
     Page<UserPreferences> findByUserId(UUID userId, Pageable pageable);
+
+    @Query("SELECT up.user.email FROM UserPreferences up WHERE up.consent = true AND up.category.isActive = true AND up.category.categoryName = :categoryName")
+    List<String> getAllEmails(String categoryName);
 }
