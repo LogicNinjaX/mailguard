95103206dd4f0d63c51d6c7c76b0894e596998a7
Nitish <mail.logicninjax@gmail.com>
2025-07-24 11:18:14 +0530

    implement admin service method

--- src/main/java/com/example/mailguard/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/mailguard/service/impl/AdminServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.mailguard.service.impl;
 
+import com.example.mailguard.repository.EmailCategoriesRepository;
 import com.example.mailguard.repository.UserPreferencesRepository;
 import com.example.mailguard.service.AdminService;
 import com.example.mailguard.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @Service
@@ -13,10 +17,13 @@ public class AdminServiceImpl implements AdminService {
 
     private final UserPreferencesRepository userPreferencesRepository;
     private final EmailService emailService;
+    private final EmailCategoriesRepository emailCategoriesRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminServiceImpl.class);
 
-    public AdminServiceImpl(UserPreferencesRepository userPreferencesRepository, EmailService emailService) {
+    public AdminServiceImpl(UserPreferencesRepository userPreferencesRepository, EmailService emailService, EmailCategoriesRepository emailCategoriesRepository) {
         this.userPreferencesRepository = userPreferencesRepository;
         this.emailService = emailService;
+        this.emailCategoriesRepository = emailCategoriesRepository;
     }
 
     public void sendEmailByCategory(String categoryName){
@@ -26,5 +33,14 @@ public class AdminServiceImpl implements AdminService {
             emailService.sendBulkEmail(userEmails, "Test message", "Email sent from spring boot");
         }
     }
+
+    @Override
+    public void changeCategoryStatus(boolean activate, UUID categoryId) {
+        int updatedRows = emailCategoriesRepository.updateActivationStatus(activate, categoryId);
+        if (updatedRows > 0){
+            LOGGER.info("Category status updated successfully for categoryId= {}", categoryId);
+        }
+    }
+
 
 }
