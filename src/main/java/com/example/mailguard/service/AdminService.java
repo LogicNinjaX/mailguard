3cf4743cbd201fa2b273ccf141921f325325f079
Nitish <mail.logicninjax@gmail.com>
2025-07-24 11:17:52 +0530

    add service method for admin

--- src/main/java/com/example/mailguard/service/AdminService.java
+++ src/main/java/com/example/mailguard/service/AdminService.java
@@ -1,6 +1,10 @@
 package com.example.mailguard.service;
 
+import java.util.UUID;
+
 public interface AdminService {
 
     void sendEmailByCategory(String categoryName);
+
+    void changeCategoryStatus(boolean activate, UUID categoryId);
 }
