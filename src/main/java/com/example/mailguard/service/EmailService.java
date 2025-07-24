2b87f1092d66927ae3041f5ed471cf65b471842f
Nitish <mail.logicninjax@gmail.com>
2025-07-24 10:35:51 +0530

    update interface

--- src/main/java/com/example/mailguard/service/EmailService.java
+++ src/main/java/com/example/mailguard/service/EmailService.java
@@ -3,7 +3,5 @@ package com.example.mailguard.service;
 import java.util.List;
 
 public interface EmailService {
-    void sendEmail(String email, String subject, String body);
-
     void sendBulkEmail(List<String> emails, String subject, String body);
 }
