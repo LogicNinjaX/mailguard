71efb5657dcf3791ed256553ef9df29d4cab91b6
Nitish <mail.logicninjax@gmail.com>
2025-07-26 08:32:30 +0530

    add open api definition using anno

--- src/main/java/com/example/mailguard/MailguardApplication.java
+++ src/main/java/com/example/mailguard/MailguardApplication.java
@@ -1,10 +1,15 @@
 package com.example.mailguard;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@OpenAPIDefinition(
+		servers = @Server(url = "http://localhost:8080", description = "Local dev server")
+)
 @SpringBootApplication
 @EnableMethodSecurity
 @EnableAsync
