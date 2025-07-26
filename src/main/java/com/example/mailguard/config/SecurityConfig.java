ff606add86f00f577924785182e2f2aac15f6c7b
Nitish <mail.logicninjax@gmail.com>
2025-07-26 09:39:03 +0530

    allow swagger urls

--- src/main/java/com/example/mailguard/config/SecurityConfig.java
+++ src/main/java/com/example/mailguard/config/SecurityConfig.java
@@ -27,12 +27,17 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/v1/auth/**").permitAll()
+                        auth.requestMatchers(
+                              "/api/v1/auth/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html"
+                                ).permitAll()
                                 .anyRequest().authenticated()
                 ).addFilterBefore(
                         new JwtAuthenticationFilter(jwtTokenUtil, userDetailsService),
@@ -43,7 +48,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
 
